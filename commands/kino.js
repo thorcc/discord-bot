d16bf2acd3f1cdfc70ef6ea09e4ae4b2169a4fee
thorcc <thorchrcow@gmail.com>
2021-11-02 14:22:22 +0100

    add embedded posters

--- commands/kino.js
+++ commands/kino.js
@@ -1,14 +1,15 @@
 const axios = require("axios");
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 
 const getVegaToday = async () => {
     const url = "https://vegascene.no/template/ajax/json_showsAllDays.jsp?showType=kino";
     const { data } = await axios.get(url);
     const movies = data.dates[0].movies.map(m => {
         const showings = m.shows.map(s => s.time)
-        return `${m.title}: ${showings.join(', ')}`;
-    }).join('\n');
-    return `Vega kino - Dagens program \n${movies}`;
+        return {name: m.title, value: showings.join(', ')};
+    });
+    return movies;
 }
 
 module.exports = {
@@ -16,7 +17,22 @@ module.exports = {
 		.setName('kino')
 		.setDescription('Henter dagens kinoprogram'),
 	async execute(interaction) {
-        const program = await getVegaToday();
-		await interaction.reply(program);
+        const movies = await getVegaToday();
+        const date = new Date().toLocaleDateString('no-no')
+        
+        const moviesEmbed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Kinoprogram')
+            .setURL('https://vegascene.no/vegascene/program/?date=ALLE&type=ALLE')
+            .setAuthor('Christian Strand', 'https://upload.wikimedia.org/wikipedia/commons/4/40/Til_bords_med_fienden_-_NMD_2013_%288723105666%29_%28cropped%29.jpg', 'https://no.wikipedia.org/wiki/Christian_Strand')
+            .setDescription(`${date}`)
+            .setThumbnail('https://vegascene.no/template/static/gfx/header_logo.jpg')
+            .addFields(
+                movies
+            )
+            .setTimestamp()
+        
+        await interaction.reply({ embeds: [moviesEmbed] });
+		//await interaction.reply(program);
 	},
 };
